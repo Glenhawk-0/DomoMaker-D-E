6b778a6a7967a1019bdb7598b45967563384411c
Lennon-0 <LennonEhare@Gmail.com>
2025-11-11 13:00:46 -0500

    on step 38 DomoD

--- client/maker.jsx
+++ client/maker.jsx
@@ -19,3 +19,62 @@ const handleDomo = (e, onDomoAdded) => {
     helper.sendPost(e.target.action, {name, age}, onDomoAdded);
     return false;
 }
+
+//36
+const DomoForm = (props) => {
+    return(
+        <form id="domoForm"
+        onSubmit={(e) => handleDomo(e, props.triggerReload)}
+        name="domoForm"
+        action="/maker"
+        method="POST"
+        className="domoForm"
+>
+        <label htmlFor="name">Name:  </label>
+        <input id="domoName" type="text" name="name" placeholder="Domo Name" />
+        <label htmlFor="age">Age:  </label>
+        <input id="domoAge" type="number" min="0" name="age" />
+        <input className="makeDomoSubmit" type="submit" value="Make Domo" />
+</form>
+
+    );
+};
+
+//37 
+const DomoList = (props) => {
+    const [domos, setDomos] = useState(props.domos); 
+
+    useEffect(() => {
+        const loadDomosFromServer = async () => {
+            const response = await fetch ('/getDomos');
+            setDomos(data.domos);
+
+        };
+        loadDomosFromServer();
+    }, [props.reloadDomos]);
+
+    if (domos.length === 0) {
+        return (
+            <div className="domoList">
+                <h3 className="emptyDomo">No Domos Yet!</h3>
+            </div>
+        );
+    }
+
+    const domoNodes = domos.map(domo => {
+        return(
+            <div key={domo.id} className="domo">
+                <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
+                <h3 className="domoName">Name: {domo.name}</h3>
+                <h3 className="domoAge">Age: {domo.age}</h3>
+            </div>
+        );
+    });
+    
+    return(
+        <div className="domoList">
+            {domoNodes}
+        </div>
+    );
+
+};//end of DomoList
