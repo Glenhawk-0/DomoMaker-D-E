3b1560d9dd4573d3de3edb93108efd74447d9cd1
Lennon-0 <LennonEhare@Gmail.com>
2025-11-11 11:54:48 -0500

    on step 32 domomakerD

--- client/login.jsx
+++ client/login.jsx
@@ -66,4 +66,48 @@ const LoginWindow = (props) => {
 };
 
 //21
+const SignupWindow = (props) => {
+    return(
+    <form id="signupForm"
+        name="signupForm"
+        onSubmit={handleSignup}
+        action="/signup"
+        method="POST"
+        className="mainForm"
+    >
+            <label htmlFor="username">Username: </label>
+            <input id="user" type="text" name="username" placeholder="username" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass" type="password" name="pass" placeholder="password" />
+            <label htmlFor="pass">Password: </label>
+            <input id="pass2" type="password" name="pass2" placeholder="retype password" />
+            <input className="formSubmit" type="submit" value="Sign in" />
+    </form>
+
+    );
+};
+
+//22
+const init = () => {
+    const loginButton = document.getElementById('loginButton');
+    const signupButton = document.getElementById('signupButton');
+
+    const root = createRoot(document.getElementById('content'));
+
+    loginButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        root.render(<LoginWindow /> );
+        return false;
+    });
+
+    signupButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        root.render( <SignupWindow /> );
+        return false;
+    });
+
+    root.render( <LoginWindow /> );
+};
+
+window.onload = init;
 
